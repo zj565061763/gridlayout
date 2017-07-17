df14d18d82712598db1c930f81035ab99f8a3b6d
zjun <565061763@qq.com>
2017-07-17 14:44:38 +0800

    demo修改

--- app/src/main/java/com/fanwe/www/gridlayout/MainActivity.java
+++ app/src/main/java/com/fanwe/www/gridlayout/MainActivity.java
@@ -1,19 +1,20 @@
 package com.fanwe.www.gridlayout;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
+import com.fanwe.library.SDLibrary;
+import com.fanwe.library.activity.SDBaseActivity;
 import com.fanwe.library.gridlayout.SDGridLayout;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends SDBaseActivity
 {
 
     private SDGridLayout view_grid;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    protected void init(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
+        SDLibrary.getInstance().init(getApplication());
         setContentView(R.layout.activity_main);
         view_grid = (SDGridLayout) findViewById(R.id.view_grid);
 
