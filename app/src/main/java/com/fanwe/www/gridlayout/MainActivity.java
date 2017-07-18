eceaafddee0ce89e9bb79305ba65d602221e2db3
zjun <565061763@qq.com>
2017-07-18 09:27:17 +0800

    demo测试

--- app/src/main/java/com/fanwe/www/gridlayout/MainActivity.java
+++ app/src/main/java/com/fanwe/www/gridlayout/MainActivity.java
@@ -1,47 +1,27 @@
 package com.fanwe.www.gridlayout;
 
 import android.os.Bundle;
-import android.widget.GridView;
 
 import com.fanwe.library.SDLibrary;
 import com.fanwe.library.activity.SDBaseActivity;
 import com.fanwe.library.gridlayout.SDGridLayout;
-import com.fanwe.library.listener.SDSimpleIterateCallback;
-import com.fanwe.library.model.SelectableModel;
-import com.fanwe.library.utils.SDCollectionUtil;
 import com.fanwe.www.gridlayout.adapter.ListViewAdapter;
-import com.fanwe.www.gridlayout.adapter.ListViewListAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends SDBaseActivity
 {
 
-    private GridView view_grid;
+    private SDGridLayout view_grid;
 
     @Override
     protected void init(Bundle savedInstanceState)
     {
         SDLibrary.getInstance().init(getApplication());
         setContentView(R.layout.activity_main);
-        view_grid = (GridView) findViewById(R.id.view_grid);
+        view_grid = (SDGridLayout) findViewById(R.id.view_grid);
 
-        final List<List<DataModel>> listModel = new ArrayList<>();
+        ListViewAdapter adapter = new ListViewAdapter(DataModel.get(100), this);
 
-        SDCollectionUtil.foreach(30, new SDSimpleIterateCallback()
-        {
-            @Override
-            public boolean next(int i)
-            {
-                listModel.add(DataModel.get(10));
-                return false;
-            }
-        });
-        ListViewListAdapter adapter = new ListViewListAdapter(listModel, this);
-
-
-        view_grid.setNumColumns(1);
+        view_grid.setNumColumns(3);
         view_grid.setVerticalSpacing(10);
         view_grid.setHorizontalSpacing(10);
         view_grid.setAdapter(adapter);
