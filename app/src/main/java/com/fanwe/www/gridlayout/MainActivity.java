ba9ed3b98db08c2816be23619a070d1b7ece2b1f
zjun <565061763@qq.com>
2017-07-19 19:06:58 +0800

    修改demo

--- app/src/main/java/com/fanwe/www/gridlayout/MainActivity.java
+++ app/src/main/java/com/fanwe/www/gridlayout/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends SDBaseActivity
 
         ListViewAdapter adapter = new ListViewAdapter(DataModel.get(10), this);
 
-        view_grid.setSpanCount(3); //设置列数
+        view_grid.setSpanCount(3); //设置行或者列的网格数量
         view_grid.setVerticalSpacing(10); //设置竖直方向的Item间隔
         view_grid.setHorizontalSpacing(10); //设置水平方向的Item间隔
         view_grid.setAdapter(adapter); //设置适配器绑定数据
